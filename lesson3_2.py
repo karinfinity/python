6c91553cc6c1960963b9821e580153844200ce91
karinfinity <karinfinity@gmail.com>
2020-07-06 00:01:54 +0300

    Практические задание от 02.07.2020

---
+++ lesson3_2.py
@@ -0,0 +1,10 @@
+# Task 2
+name = input('enter name')
+surname = input('enter surname')
+year = int(input('enter year'))
+city = input('enter city')
+email = input('enter email')
+telephone = input('input telephone')
+
+def my_func (name, surname, year, city, email, telephone):
+    print(name, surname, year, city, email, telephone)
